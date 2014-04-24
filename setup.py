63aa9a75a026531d74f44506b1fb052329d2ad54
Norbert Crombach <norbert.crombach@primetheory.org>
2014-04-24 17:49:54 +0200

    Import Apache license and setuptools configuration

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name='twokenize',
+      version='0.0.1',
+      maintainer='Norbert Crombach',
+      maintainer_email='norbert.crombach@primetheory.org',
+      packages=['twokenize'])
